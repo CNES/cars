2ec4c3c35323d8345956e3b82927492fdbf33ad1
David Youssefi <david.youssefi@cnes.fr>
2020-12-04 19:06:42 +0100

    fix: pylint/C0301-line-too-long test_notebooks.py

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -50,23 +50,30 @@ def test_step_by_step_compute_dsm():
     #     check_inputs=True)
 
     with tempfile.TemporaryDirectory(dir=temporary_dir()) as directory:
-        subprocess.run(['jupyter nbconvert --to script {}/notebooks/step_by_step_compute_dsm.ipynb --output-dir {}'
+        subprocess.run(['jupyter nbconvert '
+                        '--to script '
+                        '{}/notebooks/step_by_step_compute_dsm.ipynb '
+                        '--output-dir {}'
                        .format(cars_path, directory)], shell=True)
 
-        for line in fileinput.input('{}/step_by_step_compute_dsm.py'.format(directory), inplace=True):
+        for line in fileinput.input(
+            '{}/step_by_step_compute_dsm.py'.format(directory), inplace=True):
             if "cars_home = \"TODO\"" in line:
                 line = line.replace("TODO", cars_path)
             elif "content_dir = \"TODO\"" in line:
                 line = line.replace("TODO",
-                                    absolute_data_path("input/notebooks_input/content_dir/"))
+                                    absolute_data_path(
+                                        "input/notebooks_input/content_dir/"))
             elif "roi_file = \"TODO\"" in line:
-                line = line.replace("TODO", absolute_data_path("input/notebooks_input/content_dir/"
-                                                               "envelopes_intersection.gpkg"))
+                line = line.replace("TODO", absolute_data_path(
+                    "input/notebooks_input/content_dir/"
+                    "envelopes_intersection.gpkg"))
             elif "output_dir = \"TODO\"" in line:
                 line = line.replace("TODO", directory)
             print(line) # keep this print
 
-        out = subprocess.run(['ipython {}/step_by_step_compute_dsm.py'.format(directory)], shell=True)
+        out = subprocess.run(['ipython {}/step_by_step_compute_dsm.py'
+        .format(directory)], shell=True)
 
         out.check_returncode()
 
@@ -93,18 +100,23 @@ def test_epipolar_distributions():
     #     check_inputs=True)
 
     with tempfile.TemporaryDirectory(dir=temporary_dir()) as directory:
-        subprocess.run(['jupyter nbconvert --to script {}/notebooks/epipolar_distributions.ipynb --output-dir {}'
+        subprocess.run(['jupyter nbconvert '
+                        '--to script {}/notebooks/epipolar_distributions.ipynb '
+                        '--output-dir {}'
                        .format(cars_path, directory)], shell=True)
 
-        for line in fileinput.input('{}/epipolar_distributions.py'.format(directory), inplace=True):
+        for line in fileinput.input('{}/epipolar_distributions.py'
+        .format(directory), inplace=True):
             if "cars_home = \"TODO\"" in line:
                 line = line.replace("TODO", cars_path)
             elif "content_dir = \"TODO\"" in line:
                 line = line.replace("TODO",
-                                    absolute_data_path("input/notebooks_input/content_dir/"))
+                                    absolute_data_path(
+                                        "input/notebooks_input/content_dir/"))
             print(line)  # keep this print
 
-        out = subprocess.run(['ipython {}/epipolar_distributions.py'.format(directory)], shell=True)
+        out = subprocess.run(['ipython {}/epipolar_distributions.py'
+        .format(directory)], shell=True)
 
         out.check_returncode()
 
@@ -114,18 +126,23 @@ def test_lowres_dem_fit():
     cars_path = os.environ.get('CARSPATH')
 
     with tempfile.TemporaryDirectory(dir=temporary_dir()) as directory:
-        subprocess.run(['jupyter nbconvert --to script {}/notebooks/lowres_dem_fit.ipynb --output-dir {}'
+        subprocess.run(['jupyter nbconvert '
+                        '--to script {}/notebooks/lowres_dem_fit.ipynb '
+                        '--output-dir {}'
                        .format(cars_path, directory)], shell=True)
 
-        for line in fileinput.input('{}/lowres_dem_fit.py'.format(directory), inplace=True):
+        for line in fileinput.input('{}/lowres_dem_fit.py'.format(directory),
+                                    inplace=True):
             if "cars_home = \"TODO\"" in line:
                 line = line.replace("TODO", cars_path)
             elif "content_dir = \"TODO\"" in line:
                 line = line.replace("TODO",
-                                    absolute_data_path("input/notebooks_input/lowres_dem/"))
+                                    absolute_data_path(
+                                        "input/notebooks_input/lowres_dem/"))
             print(line)  # keep this print
 
-        out = subprocess.run(['ipython {}/lowres_dem_fit.py'.format(directory)], shell=True)
+        out = subprocess.run(['ipython {}/lowres_dem_fit.py'.format(directory)],
+                             shell=True)
 
         out.check_returncode()
 
@@ -152,15 +169,21 @@ def test_compute_dsm_memory_monitoring():
     #     check_inputs=True)
 
     with tempfile.TemporaryDirectory(dir=temporary_dir()) as directory:
-        subprocess.run(['jupyter nbconvert --to script {}/notebooks/compute_dsm_memory_monitoring.ipynb --output-dir {}'
+        subprocess.run(['jupyter nbconvert '
+                        '--to script '
+                        '{}/notebooks/compute_dsm_memory_monitoring.ipynb '
+                        '--output-dir {}'
                        .format(cars_path, directory)], shell=True)
 
-        for line in fileinput.input('{}/compute_dsm_memory_monitoring.py'.format(directory), inplace=True):
+        for line in fileinput.input('{}/compute_dsm_memory_monitoring.py'
+        .format(directory), inplace=True):
             if "compute_dsm_output_dir = \"TODO\"" in line:
                 line = line.replace("TODO",
-                                    absolute_data_path("input/notebooks_input/content_dir/"))
+                                    absolute_data_path(
+                                        "input/notebooks_input/content_dir/"))
             print(line)  # keep this print
 
-        out = subprocess.run(['ipython {}/compute_dsm_memory_monitoring.py'.format(directory)], shell=True)
+        out = subprocess.run(['ipython {}/compute_dsm_memory_monitoring.py'
+        .format(directory)], shell=True)
 
         out.check_returncode()
