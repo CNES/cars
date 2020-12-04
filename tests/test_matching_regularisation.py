1637b68fc8cde7181dba76079b313956b7a56e78
David Youssefi <david.youssefi@cnes.fr>
2020-12-04 19:06:42 +0100

    fix: pylint/C0301-line-too-long test_matching_regularisation.py

--- tests/test_matching_regularisation.py
+++ tests/test_matching_regularisation.py
@@ -61,7 +61,8 @@ def ref_ds(ref_row, ref_col):
 
     ds = xr.Dataset({
         cst.EPI_MSK: ([cst.ROW, cst.COL], ref_mc_msk)
-    }, coords={cst.ROW: np.array(range(ref_row)), cst.COL: np.array(range(ref_col))})
+    }, coords={cst.ROW: np.array(range(ref_row)),
+               cst.COL: np.array(range(ref_col))})
 
     return ds
 
@@ -75,7 +76,8 @@ def sec_ds(sec_row, sec_col):
 
     ds = xr.Dataset({
         cst.EPI_MSK: ([cst.ROW, cst.COL], sec_mc_msk)
-    }, coords={cst.ROW: np.array(range(sec_row)), cst.COL: np.array(range(sec_col))})
+    }, coords={cst.ROW: np.array(range(sec_row)),
+               cst.COL: np.array(range(sec_col))})
 
     return ds
 
@@ -85,13 +87,16 @@ def ref_disp(ref_row, ref_col):
     ref_disp = np.arange(ref_row * ref_col, dtype=np.int16)
     ref_disp = ref_disp.reshape((ref_row, ref_col))
 
-    ref_disp_msk = np.full((ref_row, ref_col), fill_value=255, dtype=np.uint16)
+    ref_disp_msk = np.full((ref_row, ref_col),
+                           fill_value=255,
+                           dtype=np.uint16)
     ref_disp_msk[2, :] = 0  # last column set to unvalid data
 
     disp_ds = xr.Dataset({
         cst.DISP_MAP: ([cst.ROW, cst.COL], ref_disp),
         cst.DISP_MSK: ([cst.ROW, cst.COL], ref_disp_msk)
-    }, coords={cst.ROW: np.array(range(ref_row)), cst.COL: np.array(range(ref_col))})
+    }, coords={cst.ROW: np.array(range(ref_row)),
+               cst.COL: np.array(range(ref_col))})
 
     return disp_ds
 
@@ -107,7 +112,8 @@ def sec_disp(sec_row, sec_col):
     disp_ds = xr.Dataset({
         cst.DISP_MAP: ([cst.ROW, cst.COL], sec_disp),
         cst.DISP_MSK: ([cst.ROW, cst.COL], sec_disp_msk)
-    }, coords={cst.ROW: np.array(range(sec_row)), cst.COL: np.array(range(sec_col))})
+    }, coords={cst.ROW: np.array(range(sec_row)),
+               cst.COL: np.array(range(sec_col))})
 
     return disp_ds
 
@@ -123,12 +129,17 @@ def test_update_disp_to_0_no_tags_in_jsons(ref_ds, sec_ds, ref_disp, sec_disp):
 
     # test
     disp_no_tags_in_json = deepcopy(disp)
-    matching_regularisation.update_disp_to_0(disp_no_tags_in_json, ref_ds, sec_ds,
-                         absolute_data_path("input/matching_regularisation_input/mask_no_set_to_ref_alt_classes.json"),
-                         absolute_data_path("input/matching_regularisation_input/mask_no_set_to_ref_alt_classes.json"))
+    matching_regularisation.update_disp_to_0(
+        disp_no_tags_in_json, ref_ds, sec_ds,
+        absolute_data_path(
+    "input/matching_regularisation_input/mask_no_set_to_ref_alt_classes.json"),
+        absolute_data_path(
+    "input/matching_regularisation_input/mask_no_set_to_ref_alt_classes.json"))
 
-    assert_same_datasets(disp_no_tags_in_json[cst.STEREO_REF], disp[cst.STEREO_REF])
-    assert_same_datasets(disp_no_tags_in_json[cst.STEREO_SEC], disp[cst.STEREO_SEC])
+    assert_same_datasets(disp_no_tags_in_json[cst.STEREO_REF],
+                         disp[cst.STEREO_REF])
+    assert_same_datasets(disp_no_tags_in_json[cst.STEREO_SEC],
+                         disp[cst.STEREO_SEC])
 
 
 @pytest.mark.unit_tests
@@ -142,32 +153,43 @@ def test_update_disp_0(ref_ds, sec_ds, ref_disp, sec_disp):
 
     # test
     matching_regularisation.\
-        update_disp_to_0(disp, ref_ds, sec_ds,
-                         absolute_data_path("input/matching_regularisation_input/mask1_set_to_ref_alt_classes.json"),
-                         absolute_data_path("input/matching_regularisation_input/mask2_set_to_ref_alt_classes.json"))
+        update_disp_to_0(
+            disp, ref_ds, sec_ds,
+            absolute_data_path(
+      "input/matching_regularisation_input/mask1_set_to_ref_alt_classes.json"),
+            absolute_data_path(
+      "input/matching_regularisation_input/mask2_set_to_ref_alt_classes.json"))
 
     up_ref_disp = deepcopy(ref_disp[cst.DISP_MAP].values)
     up_ref_disp_msk = deepcopy(ref_disp[cst.DISP_MSK].values)
 
-    ref_msk_set_to_ref_alt = np.logical_or(np.where(ref_ds[cst.EPI_MSK].values == 1, True, False),
-                                             np.where(ref_ds[cst.EPI_MSK].values == 3, True, False))
+    ref_msk_set_to_ref_alt = np.logical_or(
+        np.where(ref_ds[cst.EPI_MSK].values == 1, True, False),
+        np.where(ref_ds[cst.EPI_MSK].values == 3, True, False))
     up_ref_disp[ref_msk_set_to_ref_alt] = 0
-    up_ref_disp_msk[np.where(ref_ds[cst.EPI_MSK].values == 3, True, False)] = 255
+    up_ref_disp_msk[np.where(ref_ds[cst.EPI_MSK].values == 3,
+                    True, False)] = 255
 
-    assert np.allclose(disp[cst.STEREO_REF][cst.DISP_MAP].values, up_ref_disp)
-    assert np.allclose(disp[cst.STEREO_REF][cst.DISP_MSK].values, up_ref_disp_msk)
+    assert np.allclose(disp[cst.STEREO_REF][cst.DISP_MAP].values,
+                       up_ref_disp)
+    assert np.allclose(disp[cst.STEREO_REF][cst.DISP_MSK].values,
+                       up_ref_disp_msk)
     assert np.allclose(disp[cst.STEREO_REF][cst.DISP_MSK_DISP_TO_0].values,
                        ref_msk_set_to_ref_alt)
 
     up_sec_disp = deepcopy(sec_disp[cst.DISP_MAP].values)
     up_sec_disp_msk = deepcopy(sec_disp[cst.DISP_MSK].values)
 
-    sec_msk_set_to_ref_alt = np.logical_or(np.where(sec_ds[cst.EPI_MSK].values == 4, True, False),
-                                             np.where(sec_ds[cst.EPI_MSK].values == 5, True, False))
+    sec_msk_set_to_ref_alt = \
+        np.logical_or(np.where(sec_ds[cst.EPI_MSK].values == 4, True, False),
+        np.where(sec_ds[cst.EPI_MSK].values == 5, True, False))
     up_sec_disp[sec_msk_set_to_ref_alt] = 0
-    up_sec_disp_msk[np.where(sec_ds[cst.EPI_MSK].values == 5, True, False)] = 255
+    up_sec_disp_msk[np.where(
+        sec_ds[cst.EPI_MSK].values == 5, True, False)] = 255
 
-    assert np.allclose(disp[cst.STEREO_SEC][cst.DISP_MAP].values, up_sec_disp)
-    assert np.allclose(disp[cst.STEREO_SEC][cst.DISP_MSK].values, up_sec_disp_msk)
+    assert np.allclose(disp[cst.STEREO_SEC][cst.DISP_MAP].values,
+                       up_sec_disp)
+    assert np.allclose(disp[cst.STEREO_SEC][cst.DISP_MSK].values,
+                       up_sec_disp_msk)
     assert np.allclose(disp[cst.STEREO_SEC][cst.DISP_MSK_DISP_TO_0].values,
                        sec_msk_set_to_ref_alt)
