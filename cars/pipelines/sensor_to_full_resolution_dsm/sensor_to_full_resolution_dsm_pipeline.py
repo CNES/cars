709b337efd890369a4f5a13dec8e761db9d6c453
steux yoann <yoann.steux@c-s.fr>
2022-10-11 17:07:31 +0000

    feat: regroup pipeline json in one folder

--- cars/pipelines/sensor_to_full_resolution_dsm/sensor_to_full_resolution_dsm_pipeline.py
+++ cars/pipelines/sensor_to_full_resolution_dsm/sensor_to_full_resolution_dsm_pipeline.py
@@ -72,7 +72,10 @@ class SensorToFullResolutionDsmPipeline(PipelineTemplate):
         # Get root package directory
         package_path = os.path.dirname(__file__)
         json_file = os.path.join(
-            package_path, "sensor_to_full_resolution_dsm.json"
+            package_path,
+            "..",
+            "conf_pipeline",
+            "sensor_to_full_resolution_dsm.json",
         )
         with open(json_file, "r", encoding="utf8") as fstream:
             pipeline_config = json.load(fstream)
