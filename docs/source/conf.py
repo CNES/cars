6f33300314886001f3759fa7ceab9cb1c14e60a5
Emmanuel Dubois <emmanuel.dubois@cnes.fr>
2022-12-19 17:02:21 +0000

    fix: clean astroid recursion limit in autoapi (align with pylint)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,9 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.setrecursionlimit(1500)
+
+# Extend Recursion limit for RecursionError in big files (bug astroid)
+sys.setrecursionlimit(8 * sys.getrecursionlimit())
 
 # -- Project information -----------------------------------------------------
 
