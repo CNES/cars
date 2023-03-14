4011b4795bc842b6499378f52754c3d9856bb56f
David Youssefi <david.youssefi@cnes.fr>
2023-03-14 14:59:34 +0000

    refac: replace otbapp by compute_matches python

--- cars/applications/sparse_matching/sparse_matching_tools.py
+++ cars/applications/sparse_matching/sparse_matching_tools.py
@@ -32,6 +32,7 @@ import os
 # Third party imports
 import numpy as np
 import pandas
+from cyvlfeat.sift.sift import sift
 
 # CARS imports
 import cars.applications.sparse_matching.sparse_matching_constants as sm_cst
@@ -45,6 +46,151 @@ from cars.core import preprocessing, projection
 from cars.externals import otb_pipelines
 
 
+def euclidean_matrix_distance(descr1: np.array, descr2: np.array):
+    """Compute a matrix containing cross euclidean distance
+    :param descr1: first keypoints descriptor
+    :type descr1: numpy.ndarray
+    :param descr2: second keypoints descriptor
+    :type descr2: numpy.ndarray
+    :return euclidean matrix distance
+    :rtype: float
+    """
+    sq_descr1 = np.sum(descr1**2, axis=1)[:, np.newaxis]
+    sq_descr2 = np.sum(descr2**2, axis=1)
+    dot_descr12 = np.dot(descr1, descr2.T)
+    return np.sqrt(sq_descr1 + sq_descr2 - 2 * dot_descr12)
+
+
+def convert_dog_threshold(n_scale_per_octave, dog_threshold):
+    """Convert dog (difference of gaussians) threshold to peak thresh
+    :param n_scale_per_octave: number of levels / octave of the DoG scale space
+    :type n_scale_per_octave: float
+    :param dog_threshold: threshold to detect keypoints in dog
+    :type dog_threshold: float
+    """
+    k_nspo = np.exp(np.log(2) / n_scale_per_octave)
+    k_3 = np.exp(np.log(2) / 3.0)
+    return (k_nspo - 1.0) / (k_3 - 1.0) * dog_threshold
+
+
+def compute_matches(
+    left: np.ndarray,
+    right: np.ndarray,
+    left_mask: np.ndarray = None,
+    right_mask: np.ndarray = None,
+    left_origin: [float, float] = None,
+    right_origin: [float, float] = None,
+    matching_threshold: float = 0.6,
+    n_octave: int = 8,
+    n_scale_per_octave: int = 3,
+    dog_threshold: int = 20,
+    edge_threshold: int = 5,
+    magnification: float = 2.0,
+    backmatching: bool = True,
+):
+    """
+    Compute matches between left and right
+    Convention for masks: True is a valid pixel
+    """
+    left_origin = [0, 0] if left_origin is None else left_origin
+    right_origin = [0, 0] if right_origin is None else right_origin
+    peak_thresh = convert_dog_threshold(n_scale_per_octave, dog_threshold)
+
+    # compute keypoints + descriptors
+    left_frames, left_descr = sift(
+        left,
+        n_octaves=n_octave,
+        n_levels=n_scale_per_octave,
+        first_octave=-1,
+        peak_thresh=peak_thresh,
+        edge_thresh=edge_threshold,
+        magnification=magnification,
+        float_descriptors=True,
+        compute_descriptor=True,
+        verbose=False,
+    )
+
+    right_frames, right_descr = sift(
+        right,
+        n_octaves=n_octave,
+        n_levels=n_scale_per_octave,
+        first_octave=-1,
+        peak_thresh=peak_thresh,
+        edge_thresh=edge_threshold,
+        magnification=magnification,
+        float_descriptors=True,
+        compute_descriptor=True,
+        verbose=False,
+    )
+
+    # Filter keypoints that falls out of the validity mask (0=valid)
+    if left_mask is not None:
+        pixel_indices = np.floor(left_frames[:, 0:2]).astype(int)
+        valid_left_frames_mask = left_mask[
+            pixel_indices[:, 0], pixel_indices[:, 1]
+        ]
+        left_frames = left_frames[valid_left_frames_mask]
+        left_descr = left_descr[valid_left_frames_mask]
+
+    if right_mask is not None:
+        pixel_indices = np.floor(right_frames[:, 0:2]).astype(int)
+        valid_right_frames_mask = right_mask[
+            pixel_indices[:, 0], pixel_indices[:, 1]
+        ]
+        right_frames = right_frames[valid_right_frames_mask]
+        right_descr = right_descr[valid_right_frames_mask]
+
+    # Early return for empty frames
+    if left_frames.shape[0] == 0 or right_frames.shape[0] == 0:
+        return np.empty((0, 4))
+
+    # translate matches according image origin
+    # revert origin due to frame convention: [Y, X, S, TH] X: 1, Y: 0)
+    left_frames[..., 0:2] += left_origin[::-1]
+    right_frames[..., 0:2] += right_origin[::-1]
+
+    # compute euclidean matrix distance
+    emd = euclidean_matrix_distance(left_descr, right_descr)
+
+    # get nearest sift (regarding descriptors)
+    idx_nearest_dlr = (np.arange(np.shape(emd)[0]), np.nanargmin(emd, axis=1))
+    idx_nearest_drl = (np.nanargmin(emd, axis=0), np.arange(np.shape(emd)[1]))
+
+    # get absolute distances
+    dist_dlr = emd[idx_nearest_dlr]
+    dist_drl = emd[idx_nearest_drl]
+
+    # get relative distance (ratio to second nearest distance)
+    second_dist_dlr = np.partition(emd, 1, axis=1)[:, 1]
+    dist_dlr /= second_dist_dlr
+    second_dist_drl = np.partition(emd, 1, axis=0)[1, :]
+    dist_drl /= second_dist_drl
+
+    # stack matches which its distance
+    idx_matches_dlr = np.column_stack((*idx_nearest_dlr, dist_dlr))
+    idx_matches_drl = np.column_stack((*idx_nearest_drl, dist_drl))
+
+    # check backmatching
+    if backmatching is True:
+        back = (
+            idx_matches_dlr[:, 0]
+            == idx_matches_drl[idx_matches_dlr[:, 1].astype(int)][:, 0]
+        )
+        matches_idx = idx_matches_dlr[back]
+    else:
+        matches_idx = idx_matches_dlr
+
+    # threshold matches
+    matches_idx = matches_idx[matches_idx[:, -1] < matching_threshold, :]
+
+    # retrieve points: [Y, X, S, TH] X: 1, Y: 0
+    # fyi: ``S`` is the scale and ``TH`` is the orientation (in radians)
+    left_points = left_frames[matches_idx[:, 0].astype(int), 1::-1]
+    right_points = right_frames[matches_idx[:, 1].astype(int), 1::-1]
+    matches = np.concatenate((left_points, right_points), axis=1)
+    return matches
+
+
 def dataset_matching(
     ds1,
     ds2,
@@ -55,6 +201,7 @@ def dataset_matching(
     edge_threshold=5,
     magnification=2.0,
     backmatching=True,
+    otb=False,
 ):
     """
     Compute sift matches between two datasets
@@ -85,23 +232,44 @@ def dataset_matching(
     roi2 = [0, 0, size2[0], size2[1]]
     origin2 = [float(ds2.attrs["region"][0]), float(ds2.attrs["region"][1])]
 
-    matches = otb_pipelines.epipolar_sparse_matching(
-        ds1,
-        roi1,
-        size1,
-        origin1,
-        ds2,
-        roi2,
-        size2,
-        origin2,
-        matching_threshold,
-        n_octave,
-        n_scale_per_octave,
-        dog_threshold,
-        edge_threshold,
-        magnification,
-        backmatching,
-    )
+    if otb is False:
+        left = ds1.im.values
+        right = ds2.im.values
+        left_mask = ds1.msk.values == 0
+        right_mask = ds2.msk.values == 0
+        matches = compute_matches(
+            left,
+            right,
+            left_mask=left_mask,
+            right_mask=right_mask,
+            left_origin=origin1,
+            right_origin=origin2,
+            matching_threshold=matching_threshold,
+            n_octave=n_octave,
+            n_scale_per_octave=n_scale_per_octave,
+            dog_threshold=dog_threshold,
+            edge_threshold=edge_threshold,
+            magnification=magnification,
+            backmatching=backmatching,
+        )
+    else:
+        matches = otb_pipelines.epipolar_sparse_matching(
+            ds1,
+            roi1,
+            size1,
+            origin1,
+            ds2,
+            roi2,
+            size2,
+            origin2,
+            matching_threshold,
+            n_octave,
+            n_scale_per_octave,
+            dog_threshold,
+            edge_threshold,
+            magnification,
+            backmatching,
+        )
 
     return matches
 
