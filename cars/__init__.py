e499e7a281895ab9dc7f78dd873ee466c9b7a224
Mathis ROUX <mathis.roux@csgroup.eu>
2024-07-17 08:00:49 +0000

    feat: disable dask crash due to memory overload

--- cars/__init__.py
+++ cars/__init__.py
@@ -36,6 +36,14 @@ except Exception:  # pylint: disable=broad-except
 __author__ = "CNES"
 __email__ = "cars@cnes.fr"
 
+# Force the use of CARS dask configuration
+dask_config_path = os.path.join(
+    os.path.dirname(__file__), "orchestrator", "cluster", "dask_config"
+)
+if not os.path.isdir(dask_config_path):
+    raise NotADirectoryError("Wrong dask config path")
+os.environ["DASK_CONFIG"] = str(dask_config_path)
+
 # Force monothread for child workers
 os.environ["PANDORA_NUMBA_PARALLEL"] = str(False)
 os.environ["SHARELOC_NUMBA_PARALLEL"] = str(False)
@@ -43,7 +51,6 @@ os.environ["OMP_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
 os.environ["NUMBA_NUM_THREADS"] = "1"
 os.environ["GDAL_NUM_THREADS"] = "1"
-os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = "1"
 
 # Limit GDAL cache per worker to 500MB
 os.environ["GDAL_CACHEMAX"] = "500"
