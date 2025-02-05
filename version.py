5ed0b896800c9b6ad00d98abfd55a96d449b2527
Marian Rassat <marian.rassat@csgroup.eu>
2025-02-05 15:31:42 +0100

    fix: replaced version.sh by a cross platform python script, fixed github ci's pip install

---
+++ version.py
@@ -0,0 +1,52 @@
+import os
+import subprocess
+import sys
+
+
+def get_vcs():
+    subprocess.run(["python", "-m", "setuptools_scm"], check=True)
+
+
+def set_dist(version):
+    meson_project_dist_root = os.getenv("MESON_PROJECT_DIST_ROOT")
+    meson_rewrite = os.getenv("MESONREWRITE")
+
+    if not meson_project_dist_root or not meson_rewrite:
+        print(
+            "Error: Required environment variables MESON_PROJECT_DIST_ROOT or MESONREWRITE are missing."
+        )
+        sys.exit(1)
+
+    print(meson_project_dist_root)
+    subprocess.run(
+        [
+            meson_rewrite,
+            "--sourcedir=" + meson_project_dist_root,
+            "kwargs",
+            "set",
+            "project",
+            "/",
+            "version",
+            version,
+        ],
+        check=True,
+    )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: script.py <get-vcs | set-dist> [version]")
+        sys.exit(1)
+
+    command = sys.argv[1]
+
+    if command == "get-vcs":
+        get_vcs()
+    elif command == "set-dist":
+        if len(sys.argv) < 3:
+            print("Error: Missing version argument for set-dist.")
+            sys.exit(1)
+        set_dist(sys.argv[2])
+    else:
+        print("Error: Invalid command.")
+        sys.exit(1)
