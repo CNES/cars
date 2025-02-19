2df3722e0d87fc895bd2fcab03727992873e8827
Roux Mathis <mathis.roux@csgroup.eu>
2025-02-19 17:25:42 +0100

    fix: remove retrocompatibility with old pipelines

--- cars/cars.py
+++ cars/cars.py
@@ -111,23 +111,6 @@ def main_cli(args, dry_run=False):  # noqa: C901
 
         config_json_dir = os.path.abspath(os.path.dirname(args.conf))
         pipeline_name = config.get("advanced", {}).get("pipeline", "default")
-        old_pipelines = [
-            "sensors_to_dense_dsm",
-            "sensors_to_dense_dsm_no_merging",
-            "sensors_to_dense_depth_maps",
-            "sensors_to_dense_point_clouds",
-            "dense_point_clouds_to_dense_dsm",
-            "dense_point_clouds_to_dense_dsm_no_merging",
-        ]
-        if pipeline_name in old_pipelines:
-            warnings.warn(
-                f"The 'pipeline' value {pipeline_name} corresponds to"
-                " an old pipeline."
-                "Using the default CARS pipeline instead.",
-                FutureWarning,
-                stacklevel=2,
-            )
-            pipeline_name = "default"
 
         # Logging configuration with args Loglevel
         loglevel = getattr(args, "loglevel", "PROGRESS").upper()
