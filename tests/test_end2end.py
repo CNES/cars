86a858fb263f5218573faf4fadba83073b74eb3a
Roux Mathis <mathis.roux@csgroup.eu>
2025-09-18 17:43:35 +0200

    test: skip ventoux_unique test

--- tests/test_end2end.py
+++ tests/test_end2end.py
@@ -985,6 +985,9 @@ def test_end2end_ventoux_unique():
        check used conf reentry
     2 run dense dsm pipeline + Baseline checking
     """
+    pytest.skip(
+        reason="This test is temporarily skipped to reduce testing time"
+    )
 
     with tempfile.TemporaryDirectory(dir=temporary_dir()) as directory:
         input_json = absolute_data_path("input/phr_ventoux/input.json")
