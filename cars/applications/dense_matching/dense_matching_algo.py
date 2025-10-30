fix: masking

cars/applications/dense_matching/dense_matching_algo.py
@@ -262,7 +262,7 @@ def compute_disparity(  # pylint: disable=too-many-positional-arguments
         left_dataset[cst.EPI_MSK] = np.logical_or(
             left_dataset[cst.EPI_MSK],
             np.repeat(
-                np.any(classif_values != 0, axis=0),
+                np.any(classif_values != 0, axis=0)[np.newaxis, ...],
                 left_dataset.sizes["band_im"],
                 axis=0,
             ),
@@ -279,7 +279,7 @@ def compute_disparity(  # pylint: disable=too-many-positional-arguments
         right_dataset[cst.EPI_MSK] = np.logical_or(
             right_dataset[cst.EPI_MSK],
             np.repeat(
-                np.any(classif_values != 0, axis=0),
+                np.any(classif_values != 0, axis=0)[np.newaxis, ...],
                 right_dataset.sizes["band_im"],
                 axis=0,
             ),

cars/applications/sparse_matching/sparse_matching_algo.py
@@ -322,12 +322,7 @@ def dataset_matching(  # pylint: disable=too-many-positional-arguments
         else:
             classif_values = ds1[cst.EPI_CLASSIFICATION].values
         left_mask = np.logical_and(
-            left_mask,
-            np.repeat(
-                ~np.any(classif_values > 0, axis=0),
-                ds1.sizes["band_im"],
-                axis=0,
-            ),
+            left_mask, ~np.any(classif_values > 0, axis=0)
         )
 
     if cst.EPI_CLASSIFICATION in ds2:
@@ -340,12 +335,7 @@ def dataset_matching(  # pylint: disable=too-many-positional-arguments
         else:
             classif_values = ds1[cst.EPI_CLASSIFICATION].values
         right_mask = np.logical_and(
-            right_mask,
-            np.repeat(
-                ~np.any(classif_values > 0, axis=0),
-                ds2.sizes["band_im"],
-                axis=0,
-            ),
+            right_mask, ~np.any(classif_values > 0, axis=0)
         )
 
     matches = compute_matches(