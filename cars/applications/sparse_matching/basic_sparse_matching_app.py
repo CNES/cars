92257b840b2406a040d68656c7af4e9b904428d1
Marian Rassat <marian.rassat@csgroup.eu>
2026-03-27 16:01:36 +0100

    misc: added copyright header to new files

--- cars/applications/sparse_matching/basic_sparse_matching_app.py
+++ cars/applications/sparse_matching/basic_sparse_matching_app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf8
 #
-# Copyright (c) 2020 Centre National d'Etudes Spatiales (CNES).
+# Copyright (c) 2026 Centre National d'Etudes Spatiales (CNES).
 #
 # This file is part of CARS
 # (see https://github.com/CNES/cars).
